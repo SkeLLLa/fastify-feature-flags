25b11c511f5d6e747ff5d185a80a269c7ceab49f
m03geek <s@zxc.pp.ua>
2019-04-25 15:23:04 +0300

    feat(provider): add env provider

---
+++ src/providers/env.ts
@@ -0,0 +1,34 @@
+import {GenericProvider} from './generic';
+
+export declare namespace EnvProvider {
+  interface Options extends GenericProvider.Options {
+    /**
+     * Prefix for env var filtering
+     */
+    prefix?: string;
+  }
+}
+
+export class EnvProvider extends GenericProvider {
+  private conf: {[key: string]: string | undefined};
+  constructor(options: EnvProvider.Options) {
+    super(options);
+    const {prefix} = options;
+
+    if (typeof prefix === 'string') {
+      Object.keys(process.env)
+        .filter((key) => {
+          return key.startsWith(prefix);
+        })
+        .forEach((key) => {
+          this.conf[key] = process.env[key];
+        });
+    } else {
+      this.conf = process.env;
+    }
+  }
+
+  async isEnabled(feature: string): Promise<boolean> {
+    return this.conf[feature] === 'true' || this.conf[feature] === '1';
+  }
+}
