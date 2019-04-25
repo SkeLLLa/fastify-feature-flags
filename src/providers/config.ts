671b883e4d75d29aceeae78a12d9967d451208f5
m03geek <s@zxc.pp.ua>
2019-04-25 15:23:27 +0300

    fix(provider): support sitings in config provider

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -2,17 +2,26 @@ import {GenericProvider} from './generic';
 import config from 'config';
 
 export declare namespace ConfigProvider {
-  interface Options {
-    path: string;
+  interface Options extends GenericProvider.Options {
+    /**
+     * Config prefix for flags
+     */
+    prefix: string;
   }
 }
 
 export class ConfigProvider extends GenericProvider {
+  private conf: {[key: string]: boolean | string};
   constructor(options: ConfigProvider.Options) {
     super(options);
+    this.conf = config.get(`${options.prefix}`);
   }
 
   async isEnabled(feature: string): Promise<boolean> {
-    return Boolean(config.get(`${this.options.path}.${feature}`));
+    return (
+      this.conf[feature] === 'true' ||
+      this.conf[feature] === true ||
+      this.conf[feature] === '1'
+    );
   }
 }
